60d503796b8598ce9889e270921d6ea13da5dca7
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-05 12:16:03 +0530

    Time: 3 ms (71.79%), Space: 14.7 MB (50.97%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indexMap; // value -> index
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+
+            if (indexMap.count(complement)) {
+                return { indexMap[complement], i };
+            }
+
+            indexMap[nums[i]] = i;
+        }
+
+        return {}; // No pair found
+    }
+};
