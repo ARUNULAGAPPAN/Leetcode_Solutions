db1e2e63b002b085f24620db8242561c71714cc7
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-06 11:14:51 +0530

    Time: 0 ms (100%), Space: 8.4 MB (69.63%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int d=0, nd=0;
+        for(int i=1;i<=n;i++)
+        {
+          if(i%m==0)
+          {
+            d=d+i;
+          }   
+          if(i%m!=0)
+          {
+            nd=nd+i;
+          }
+        }
+        return nd-d;
+    }
+};
