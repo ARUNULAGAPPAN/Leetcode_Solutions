c5610bceaf1c7178ac73995df3a8860cc8e93210
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-07 12:58:12 +0530

    Time: 23 ms (59.97%), Space: 41.3 MB (43.17%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-      vector<string> ans;
-        int minIndexSum = INT_MAX;
-
-        for (int i = 0; i < list1.size(); i++) {
-            for (int j = 0; j < list2.size(); j++) {
-                if (list1[i] == list2[j]) {
-                    int indexSum = i + j;
-                    if (indexSum < minIndexSum) {
-                        minIndexSum = indexSum;
-                        ans.clear(); // Clear entire result safely
-                        ans.push_back(list1[i]);
-                    } else if (indexSum == minIndexSum) {
-                        ans.push_back(list1[i]);
-                    }
-                }
-            }
-        }
-
-        return ans;
+      vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        vector<string>res;
+        unordered_map<string,int>m;
+        int min = INT_MAX;
+        for(int i = 0; i < list1.size(); i++) m[list1[i]] = i;
+        for(int i = 0; i < list2.size(); i++)
+            if(m.count(list2[i]) != 0)
+                if(m[list2[i]] + i < min) 
+                   min = m[list2[i]] + i, res.clear(), res.push_back(list2[i]);
+                else if(m[list2[i]] + i == min) res.push_back(list2[i]);
+        return res;
     }
 };
