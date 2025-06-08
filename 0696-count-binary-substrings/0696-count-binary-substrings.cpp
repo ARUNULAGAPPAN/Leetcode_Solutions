56db4c406059e254d6b27dc72aa3208c59c62caf
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-08 15:06:34 +0530

    Time: 3 ms (69.72%), Space: 13.1 MB (61.09%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+     int cur =1;
+     int prev =0;
+     int res=0;
+     for(int i=1;i<s.size();i++)
+     {
+        if(s[i]==s[i-1])
+        {
+            cur++;
+        
+        }
+        else
+        {
+            res = res + min(cur,prev);
+            prev = cur;
+            cur = 1;
+        }
+     }   
+     return res + min(cur,prev);
+    }
+};
