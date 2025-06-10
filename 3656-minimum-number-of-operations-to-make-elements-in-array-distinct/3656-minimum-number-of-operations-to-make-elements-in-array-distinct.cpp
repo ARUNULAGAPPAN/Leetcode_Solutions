191467d7571776a43158d3ac0abab4c85017e36d
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-10 11:18:37 +0530

    Time: 1 ms (66.23%), Space: 27.7 MB (61.3%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -3,12 +3,14 @@ public:
     int minimumOperations(vector<int>& nums) {
         unordered_map<int, int> mp;
         int n = nums.size();
+
         for (int i = n - 1; i >= 0; --i) {
-            if (mp[nums[i]] > 0) {
+            mp[nums[i]]++;
+            if (mp[nums[i]] > 1) {
                 return i / 3 + 1;
             }
-            mp[nums[i]] = 1;
         }
+
         return 0;
     }
 };
