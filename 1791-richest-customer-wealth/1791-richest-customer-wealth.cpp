6b02549279725df6994d9b69aeb56d07e0749969
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-10 13:51:45 +0530

    Time: 0 ms (100%), Space: 11.2 MB (98.06%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+       int m=INT_MIN;
+      int sum=0;
+       for(int i=0;i<accounts.size();i++)
+       { sum=0;
+        for(int j=0;j<accounts[0].size();j++)
+       {
+      
+          sum = sum+accounts[i][j];
+       
+       }
+          m = max(m, sum);
+       } 
+       return m;
+    }
+};
