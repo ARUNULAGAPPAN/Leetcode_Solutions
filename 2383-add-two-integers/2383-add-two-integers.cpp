a0a00a81b660837897731399673fcfa997a42d17
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-11 12:55:34 +0530

    Time: 0 ms (100%), Space: 8 MB (31.59%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.cpp
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        return (int)num1+(int)num2;
+        while(num2!=0)
+        {
+           int c= num1 & num2;
+           num1 = num1 ^ num2;
+           num2 = c<<1; 
+        }
+        return num1;
     }
 };
