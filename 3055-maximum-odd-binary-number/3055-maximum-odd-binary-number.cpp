ba46b721c17600bc5e46a9d7d9e7f5ea4c73c9ae
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-11 13:37:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (14.74%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int o =0;
-        int z=0;
-        for(char c : s)
-        {
-            if(c=='1') o++;
-            else if(c=='0') z++;
-        }
-        string ans="";
-        for(int i=0;i<o-1;i++)
-        {
-            ans = ans + '1';
-        }
-        for(int i=0;i<z;i++)
-        {
-            ans = ans +'0';
-        }
-        return ans+'1';
+     int n=s.size();
+     int o = count(s.begin(),s.end(),'1');
+     int z= n-o;
+
+     return (string(o-1,'1')+string(z,'0')+'1');
     }
 };
