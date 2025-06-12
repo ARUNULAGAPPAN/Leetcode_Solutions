3bf7b544ae5d4517ea8c027e9710ba2e0a300b23
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-12 10:24:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (42.01%) - LeetHub

--- 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int countDigits(int num)
-     {
-    int x=num; int count=0;
-    while(num>0)
-    {
-        int b=num%10; num/=10;
-        if(x%b==0)count++;
-    }    
-    return count;
+    int countDigits(int num) {
+        
+        int coun=0;
+        int x=num;
+        while(num>0)
+        {
+         int b=num%10; num/=10;
+         if(x%b==0)
+          coun++;
+        }
+        return coun;
     }
 };
