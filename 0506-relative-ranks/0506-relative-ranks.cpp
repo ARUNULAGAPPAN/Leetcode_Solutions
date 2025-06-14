cc6b7463ee032cc764f6c85b440ae72b6dbe8d55
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-14 12:36:10 +0530

    Time: 7 ms (46.06%), Space: 17.3 MB (32.81%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -2,24 +2,29 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
-        vector<int>sorted = score;
-        vector<string>ans(n);
+        vector<int> sorted = score;
 
-        sort(sorted.begin(), sorted.end(),greater<int>());
-        unordered_map<int,int>rank;
+        sort(sorted.begin(), sorted.end(), greater<int>());
+        unordered_map<int, int> rank;
 
-        for(int i=0;i<n;i++)
-        {
-            rank[sorted[i]]=i;
+        // Map each score to its rank
+        for (int i = 0; i < n; i++) {
+            rank[sorted[i]] = i;
         }
-        for(int i=0;i<n;i++)
-        {
+
+        vector<string> ans; // No pre-size, we use push_back
+        for (int i = 0; i < n; i++) {
             int r = rank[score[i]];
-            if(r==0) ans[i]="Gold Medal";
-            else if(r==1) ans[i]="Silver Medal";
-            else if(r==2) ans[i]="Bronze Medal";
-            else ans[i]=to_string(r+1);
+            if (r == 0)
+                ans.push_back("Gold Medal");
+            else if (r == 1)
+                ans.push_back("Silver Medal");
+            else if (r == 2)
+                ans.push_back("Bronze Medal");
+            else
+                ans.push_back(to_string(r + 1));
         }
+
         return ans;
     }
 };
