1d0c5707fbe1f84a4c3179072aa5687995d65f27
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-25 13:59:23 +0530

    Time: 2 ms (75.6%), Space: 10.2 MB (75.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n)
-     {
-    vector<vector<string>> result;
-    vector<string>board(n,string(n,'.'));
-    solve(0,result,board,n);
-    return result;    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        solve(0, ans, board, n);
+        return ans;
     }
-    void solve(int row,vector<vector<string>> &result, vector<string> &board,int n)
-    {
-        if(row==n) {result.push_back(board); return ;}
-        for(int col=0;col<n;col++)
-        {
-            if(isSafe(col,row,board,n))
-            {
-                board[row][col]='Q';
-                solve(row+1,result,board,n);
-                board[row][col]='.';
+
+    void solve(int r, vector<vector<string>> &ans, vector<string> &board, int n) {
+        if (r == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+            if (isSafe(c, r, board, n)) {
+                board[r][c] = 'Q';
+                solve(r + 1, ans, board, n);
+                board[r][c] = '.';
             }
         }
     }
-    bool isSafe(int col, int row,vector<string>&board,int n)
-    {
-      for(int i=0;i<row;i++) if(board[i][col]=='Q') return false;
-      for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)  if(board[i][j]=='Q') return false;
-      for(int i=row-1,j=col+1;i>=0&&j<n; j++,i--)  if(board[i][j]=='Q') return false;
-      return true;
+
+    bool isSafe(int c, int r, vector<string> &board, int n) {
+        for (int i = 0; i < r; i++)
+            if (board[i][c] == 'Q') return false;
+
+        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+
+        for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q') return false;
+
+        return true;
     }
-    };
+};
