4cbf4532e7ebc812135726b757a1c606de84e651
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-29 17:50:39 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.75%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+      if(str1+str2 != str2+str1)
+      {
+        return "";
+      }  
+      int l = gcd(str1.size(),str2.size());
+      return str1.substr(0,l);
+    }
+    int gcd(int a, int b)
+    {
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+};
