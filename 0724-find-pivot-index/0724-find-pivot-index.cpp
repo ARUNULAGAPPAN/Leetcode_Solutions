b8154f2eb640a835ce4f893d4ed11ee2265725cd
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-06-29 19:06:57 +0530

    Time: 0 ms (100%), Space: 35.8 MB (28.67%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+   int ts;
+   for(int n: nums)
+   {
+    ts=ts+n;
+   }
+   int ls=0;
+   for(int i=0;i<nums.size();i++)
+   {
+    if(ls == ts-ls-nums[i])
+    {
+        return i;
+    }
+    ls = ls+nums[i];
+   }
+      return -1;
+    }
+};
