59b45215f64c8178441ce840f3c367e103e2815d
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-07-01 11:30:33 +0530

    Time: 3 ms (36.41%), Space: 8.9 MB (29.69%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+      int n=word.size();
+      int c= n;
+      for(int i=1;i<n;i++)
+      {
+        if(word[i] != word[i-1])
+        {
+            c--;
+        }
+      }
+      return c;
+    }
+};
