237d76a22520c84456f4fa2139558ae3ee8b01ac
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-07-03 19:30:43 +0530

    Time: 12 ms (32.76%), Space: 19.5 MB (59.58%) - LeetHub

---
+++ 0749-shortest-completing-word/0749-shortest-completing-word.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string shortestCompletingWord(string licensePlate, vector<string>& words) {
+        vector<int>chars(26,0);
+        string ans="";
+        for(char &ch:licensePlate){
+            if(isalpha(ch)){
+                ch=tolower(ch);
+                chars[ch-'a']++;
+            }
+        }
+        for(string &word:words){
+            vector<int>currWord(26,0);
+            for(char &ch:word){
+                currWord[ch-'a']++;
+            }
+            bool valid=true;
+            for(int i=0;i<26;i++){
+                if(currWord[i]<chars[i]){
+                    valid=false;
+                    break;
+                }
+            }
+            if(valid && (ans.empty() || word.length()<ans.length()))
+            {
+                ans=word;
+            }
+        }
+        return ans;
+    }
+};
