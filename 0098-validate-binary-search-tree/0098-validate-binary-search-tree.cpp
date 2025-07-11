8df0ef36187f1fee34df9a6aaa074652040914ee
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-07-11 07:09:43 +0530

    Time: 0 ms (100%), Space: 22 MB (18.39%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool inorder(TreeNode* root, long long &prev) {
+        if (!root) return true;
+
+        if (!inorder(root->left, prev)) return false;
+
+        if (root->val <= prev) return false; // BST does not allow duplicates
+        prev = root->val;
+
+        return inorder(root->right, prev);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        long long prev = LLONG_MIN;
+        return inorder(root, prev);
+    }
+};
