b7d45133196f49c6d00350c0d547641df212a32b
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-08-05 14:51:50 +0530

    Time: 0 ms (100%), Space: 48.9 MB (49.69%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int ans=fruits.size();
+
+        for(int i=0;i<fruits.size();i++)
+        {
+            for(int j=0;j<fruits.size();j++)
+            {
+                if(fruits[i] <= baskets[j])
+                {
+                    ans--;
+                    baskets[j] = 0;
+                    break;
+                }
+        }
+        }
+        return ans;
+    }
+};
