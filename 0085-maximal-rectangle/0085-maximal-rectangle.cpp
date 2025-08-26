d107b3389a1086ccd0b300a2ae3ae7e31ab13a28
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-08-26 18:03:38 +0530

    Time: 8 ms (52.29%), Space: 18.2 MB (67.16%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    int histogram(vector<int>& arr){
+        stack<int>st;
+        int maxi=0,n=arr.size();
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]){
+                int curr=arr[st.top()]; st.pop();
+                int nse=i,pse=(st.empty()) ? -1 : st.top();
+                maxi=max(maxi,curr*(nse-pse-1));
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            int curr=arr[st.top()]; st.pop();
+            int nse=n,pse=(st.empty()) ? -1 : st.top();
+            maxi=max(maxi,curr*(nse-pse-1));
+        }
+        return maxi;
+    }
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size(),res=0;
+        vector<int>height(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1') height[j]++;
+                else height[j]=0;
+            }
+            res=max(res,histogram(height));
+        }
+        return res;
+    }
+};
