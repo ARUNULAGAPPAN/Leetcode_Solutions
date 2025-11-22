070fc9222a27ffd014ededd9d87edd204eb8cdb8
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2025-11-22 09:28:01 +0530

    Time: 14 ms (82.51%), Space: 59.9 MB (61.77%) - LeetHub

---
+++ 3675-minimum-operations-to-transform-string/3675-minimum-operations-to-transform-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+       int minOperations(string s) {
+        int res = 0;
+        for (char c : s) {
+            res = max(res, ('a' + 26 - c) % 26);
+        }
+        return res;
+    }
+};
