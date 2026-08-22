c80461cbd20972b37fcfb368c0d57e140ce5ec4c
ARUN-CODAHOLIC <sarunulagappan@gmail.com>
2026-08-22 17:47:37 +0530

    Time: 2 ms (65.66%), Space: 12.1 MB (45.45%) - LeetHub

---
+++ 0893-groups-of-special-equivalent-strings/0893-groups-of-special-equivalent-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& w) {
+        unordered_set<string> st;
+        for(int i = 0;i<w.size();i++)
+        {
+            string even = "",odd = "";
+            for(int j = 0;j<w[i].size();j++)
+            {
+                if(j%2) odd+=w[i][j];
+                else even+=w[i][j];
+            }
+            sort(even.begin(),even.end());
+            sort(odd.begin(),odd.end());
+            st.insert(even+odd);
+        }
+        return st.size();
+        
+    }
+};
